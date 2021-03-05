ada07ea44449f3887214457a25d7cc165a46fd1f
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-03-04 22:13:59 -0600

    created api route to get vote total

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -32,4 +32,27 @@ router.post('/vote', ({body}, res) => {
 });
 
 
+// GET router for all votes
+router.get('/votes', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id)
+                FROM votes
+                LEFT JOIN candidates ON votes.candidate_id = candidates.id
+                LEFT JOIN parties ON candidates.party_id = parties.id
+                GROUP BY candidate_id`;
+    const params = [];
+
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+          return;
+        }
+
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+
 module.exports = router;
